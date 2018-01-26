04a7e211b7efcaa39cb3d3b5df373c1e7982c705
Mars Wong <marswong618@gmail.com>
2018-01-26 02:23:29 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,11 +1,11 @@
 def my_each(arr)
   if block_given?
+    i = 0
+    while i < arr.length
+      item = arr[i]
+      yield item
+      i += 1
+    end
+    arr
   end
-  i = 0
-  while i < arr.length
-    item = arr[i]
-    yield item
-    i += 1
-  end
-  arr
 end
